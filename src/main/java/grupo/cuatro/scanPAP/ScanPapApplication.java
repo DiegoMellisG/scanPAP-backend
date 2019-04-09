9e6e1698e1cef0ef78b0f6ee239eaa5fbdc3edfb
DiegoMellisG <diego.mellis@usach.cl>
2019-04-09 14:00:20 -0400

    First commit

---
+++ src/main/java/grupo/cuatro/scanPAP/ScanPapApplication.java
@@ -0,0 +1,13 @@
+package grupo.cuatro.scanPAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanPapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanPapApplication.class, args);
+	}
+
+}
