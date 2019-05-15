4fac6539a5d1ac7de5e009e3d8fa2d80aa750a7d
Unknown <richard.torti@usach.cl>
2019-05-15 15:20:30 -0400

    Center model

--- src/main/java/grupo/cuatro/scanPAP/model/Center.java
+++ src/main/java/grupo/cuatro/scanPAP/model/Center.java
@@ -1,4 +1,23 @@
 package grupo.cuatro.scanPAP.model;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "CENTER")
 public class Center {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="CENTER_ID", nullable = false, unique=true)
+    private long idCenter;
+
+    @Column(name="NAME", length=50)
+    private String nameCenter;
+
+    @Column(name="ADDRESS", length=50)
+    private String addressCenter;
 }
