55968de602900e054835ee0035dabb52cdf746db
Unknown <richard.torti@usach.cl>
2019-05-15 15:47:21 -0400

    Fixing bug JpaReposiroty

--- src/main/java/grupo/cuatro/scanPAP/dao/CenterDAO.java
+++ src/main/java/grupo/cuatro/scanPAP/dao/CenterDAO.java
@@ -3,8 +3,9 @@ package grupo.cuatro.scanPAP.dao;
 import grupo.cuatro.scanPAP.model.Center;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CenterDAO extends JpaRepository<Long, Center> {
+public interface CenterDAO extends JpaRepository<Center, Long> {
 
-    Center findById(long id);
-    //Center findByName(String name);
+    Center findByIdCenter(long id);
+    Center findByAddressCenter(String address);
+    Center findByNameCenter(String name);
 }
