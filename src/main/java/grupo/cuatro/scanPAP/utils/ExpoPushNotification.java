435b5b73aa239d93b5b78fdf97f92c8e1939a0ef
Diego Mellis <diego.mellis@usach.cl>
2019-06-13 01:32:38 -0400

    Demo scheduling for presentation

--- src/main/java/grupo/cuatro/scanPAP/utils/ExpoPushNotification.java
+++ src/main/java/grupo/cuatro/scanPAP/utils/ExpoPushNotification.java
@@ -22,7 +22,7 @@ public class ExpoPushNotification {
         this.patientDAO = patientDAO;
     }
 
-    @Scheduled(cron = "0 50 23 * * *")
+    @Scheduled(cron = "0 10 18 * * *")
     public void sendPushNotifications()
     {
         //OBTENEMOS LAS PACIENTES CUYA FECHA DE VIGENCIA SE ENCUENTRA VENCIDA
