211b66a0fac7d7902d9feb3b2f2c6ea416bdc268
Diego Mellis <diego.mellis@usach.cl>
2019-07-05 15:19:16 -0400

    CHG: Change official hour to notificate

--- src/main/java/grupo/cuatro/scanPAP/utils/ExpoPushNotification.java
+++ src/main/java/grupo/cuatro/scanPAP/utils/ExpoPushNotification.java
@@ -22,7 +22,7 @@ public class ExpoPushNotification {
         this.patientDAO = patientDAO;
     }
 
-    @Scheduled(cron = "0 00 15 * * *")
+    @Scheduled(cron = "0 00 19 * * *")
     public void sendPushNotifications()
     {
         //OBTENEMOS LAS PACIENTES CUYA FECHA DE VIGENCIA SE ENCUENTRA VENCIDA
