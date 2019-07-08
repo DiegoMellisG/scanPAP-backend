8f17db657e59e1a14d43c5f5d48d2b736c87d981
Diego Mellis <diego.mellis@usach.cl>
2019-07-07 22:39:20 -0400

    CHG: Still testing hour

--- src/main/java/grupo/cuatro/scanPAP/utils/ExpoPushNotification.java
+++ src/main/java/grupo/cuatro/scanPAP/utils/ExpoPushNotification.java
@@ -22,7 +22,7 @@ public class ExpoPushNotification {
         this.patientDAO = patientDAO;
     }
 
-    @Scheduled(cron = "0 00 19 * * *")
+    @Scheduled(cron = "0 45 22 * * *")
     public void sendPushNotifications()
     {
         //OBTENEMOS LAS PACIENTES CUYA FECHA DE VIGENCIA SE ENCUENTRA VENCIDA
